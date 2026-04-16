4c9a465b85b99d6bf3dbd2d7d9053f78bd02c91a
Jesusgallardooo <jgallardo@iesrafaelalberti.es>
2026-04-16 19:30:22 +0200

    capa servicio

--- src/main/kotlin/es/iesra/servicio/IReservaService.kt
+++ src/main/kotlin/es/iesra/servicio/IReservaService.kt
@@ -8,7 +8,10 @@ import es.iesra.dominio.Reserva
  * Se aplica la inversión de dependencias en la capa de presentación.
  */
 interface IReservaService {
-    fun crearReservaVuelo(descripcion: String, origen: String, destino: String, horaVuelo: String)
-    fun crearReservaHotel(descripcion: String, ubicacion: String, numeroNoches: Int)
+    fun crearReservaVuelo(descripcion: String, origen: String, destino: String, horaVuelo: String): Boolean
+    fun crearReservaHotel(descripcion: String, ubicacion: String, numeroNoches: Int): Boolean
     fun listarReservas(): List<Reserva>
+    fun eliminarReserva(id: Int): Boolean
+    fun obtenerReserva(id: Int): Reserva?
+    fun actualizarReserva(reserva: Reserva): Boolean
 }
--- src/main/kotlin/es/iesra/servicio/ReservaService.kt
+++ src/main/kotlin/es/iesra/servicio/ReservaService.kt
@@ -1,6 +1,7 @@
 package es.iesra.servicio
 
 import es.iesra.datos.IReservaRepository
+import es.iesra.dominio.Reserva
 import es.iesra.dominio.ReservaHotel
 import es.iesra.dominio.ReservaVuelo
 
@@ -10,15 +11,42 @@ import es.iesra.dominio.ReservaVuelo
  */
 class ReservaService(private val repositorio: IReservaRepository) : IReservaService {
 
-    override fun crearReservaVuelo(descripcion: String, origen: String, destino: String, horaVuelo: String) {
+    override fun crearReservaVuelo(descripcion: String, origen: String, destino: String, horaVuelo: String): Boolean {
         val reservaVuelo = ReservaVuelo.creaInstancia(descripcion, origen, destino, horaVuelo)
-        repositorio.anadir(reservaVuelo)
+        return repositorio.anadir(reservaVuelo)
     }
 
-    override fun crearReservaHotel(descripcion: String, ubicacion: String, numeroNoches: Int) {
+    override fun crearReservaHotel(descripcion: String, ubicacion: String, numeroNoches: Int): Boolean {
         val reservaHotel = ReservaHotel.creaInstancia(descripcion, ubicacion, numeroNoches)
-        repositorio.anadir(reservaHotel)
+        return repositorio.anadir(reservaHotel)
     }
 
     override fun listarReservas() = repositorio.obtenerTodas()
+
+    override fun eliminarReserva(id: Int): Boolean {
+        val reserva = repositorio.obtenerPorId(id)
+
+        if (reserva != null){
+            repositorio.eliminar(id)
+            return true
+        }else{
+            return false
+        }
+    }
+
+    override fun obtenerReserva(id: Int): Reserva? {
+        return repositorio.obtenerPorId(id)
+    }
+
+    override fun actualizarReserva(reserva: Reserva): Boolean {
+        val reservaExistente = repositorio.obtenerPorId(reserva.id)
+
+        if (reservaExistente != null){
+            repositorio.actualizar(reserva)
+            return true
+        }else{
+            return false
+        }
+    }
+
 }
